99a1662ced7b4f27a0ffc90ef5b86e4c10407707
Andrew Bell <fiixed@gmail.com>
2016-10-02 10:44:10 -0500

    added gaze interaction with Raycast

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserPointer : MonoBehaviour {
+
+    private GameObject hitObject;
+    RaycastHit hit;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit)) {
+            Debug.Log("hit");
+            hitObject = hit.transform.gameObject;
+            Interactable interactable = hitObject.GetComponent<Interactable>();
+            if (interactable != null) {
+                interactable.targeted = true;
+            }
+        }
+	}
+}
