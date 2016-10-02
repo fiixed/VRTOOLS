f3e2e767d4004a7e9f4cf102800cf1430171c7c4
Andrew Bell <fiixed@gmail.com>
2016-10-02 15:50:05 -0500

    Reticle improvements (scaling and ZTest)

--- Assets/Scripts/LaserPointer.cs
+++ Assets/Scripts/LaserPointer.cs
@@ -5,16 +5,19 @@ public class LaserPointer : MonoBehaviour {
 
     private GameObject hitObject;
     RaycastHit hit;
+    public Reticle reticle;
 	
 	// Update is called once per frame
 	void Update () {
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit)) {
-            Debug.Log("hit");
+            reticle.SetPosition(hit);
             hitObject = hit.transform.gameObject;
             Interactable interactable = hitObject.GetComponent<Interactable>();
             if (interactable != null) {
                 interactable.targeted = true;
             }
+        } else {
+            reticle.SetPosition();
         }
 	}
 }
